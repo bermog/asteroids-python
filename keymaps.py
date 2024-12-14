ad398268612f7916d5655e6a5e566cb08c751580
Damian Bermejo <code@bermog.com>
2024-12-14 01:31:14 +0100

    Add keymaps file, implement player movement and rotation

---
+++ keymaps.py
@@ -0,0 +1,7 @@
+import pygame
+
+
+KEY_FORWARD = pygame.K_w
+KEY_BACK = pygame.K_s
+KEY_LEFT = pygame.K_a
+KEY_RIGHT = pygame.K_d
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import pygame
 import constants as c
+import keymaps as k
 from circleshape import CircleShape
 
 
@@ -16,5 +17,24 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
+    def rotate(self, dt):
+        self.rotation += c.PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * c.PLAYER_SPEED * dt
+
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), c.LINE_WIDTH)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[k.KEY_FORWARD]:
+            self.move(dt)
+        if keys[k.KEY_BACK]:
+            self.move(-dt)
+        if keys[k.KEY_LEFT]:
+            self.rotate(-dt)
+        if keys[k.KEY_RIGHT]:
+            self.rotate(dt)
