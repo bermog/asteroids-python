89d3c426a8bbabec371fecea4faba42433bcbce4
Damian Bermejo <code@bermog.com>
2024-12-14 16:58:43 +0100

    Update main loop to iterate over objects in updatable and drawable groups, add player to both groups

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def main():
     print(f"Screen width: {c.SCREEN_WIDTH}")
     print(f"Screen height: {c.SCREEN_HEIGHT}")
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
     player = Player(c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
@@ -20,9 +25,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         pygame.Surface.fill(screen, "black")
-        player.update(dt)
-        player.draw(screen)
+
+        for object in updatable:
+            object.update(dt)
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(c.FPS_LIMIT) / 1000
